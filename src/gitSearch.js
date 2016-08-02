0922f481ee92e7816f9475b23aca6bcf31234e23
zhangtong1994 <602967699@qq.com>
2016-08-02 16:51:22 +0800

    axios

--- src/gitSearch.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export default function getUserInfo(){
-  return axios.get('https://api.github.com/users/zhangtong1994')
-         .then((res) =>(
-           {gitInfo:res.data}
-         ))
-}
